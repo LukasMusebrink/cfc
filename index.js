ac16c1f02a5932fd9d3089a66c3027e6299ee360
Lukas <lukas.musebrink@gmx.de>
2018-04-23 07:08:22 +0200

    first functions

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let say = function(string){
-  return console.log(string);
-}
+#!/usr/bin/env node
+var cfcYaml = require('./lib/cfc-yaml.js');
 
-exports.say = say;
+const argv = require('yargs').argv
+const _ = require('lodash');
+
+let {target, source, config } = argv;
+
+if (!_.isNil(target) && !_.isNil(source) && !_.isNil(config)) {
+  cfcYaml.compile(source, target, config);
+}else {
+  console.log(`Please fill in all required parameters\n\t--source\t[PATH] path to source file\n\t--config\t[PATH] path to configuration file\n\t--target\t[PATH] path to target file`);
+}
