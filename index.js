af9ce0b4438a54baa7604c6adb35fdbabdb6f934
Lukas <lukas.musebrink@gmx.de>
2018-05-13 22:43:18 +0200

    updated dependencies, introduced tests, working set of functions

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-let cfcYaml = require("./lib/cfcYaml.js");
 
-const { argv } = require("yargs");
+let yaml = require("./lib/yaml.js");
+
+const {
+  argv
+} = require("yargs");
 const _ = require("lodash");
 
-let { target, source, config } = argv;
+let {
+  target,
+  source,
+  config
+} = argv;
 
 if (!_.isNil(target) && !_.isNil(source) && !_.isNil(config)) {
-  cfcYaml.compile(source, target, config);
-}else {
+  yaml.compile(source, target, config);
+} else {
   console.log("Please fill in all required parameters\n\t--source\t[PATH] path to source file\n\t--config\t[PATH] path to configuration file\n\t--target\t[PATH] path to target file");
 }
